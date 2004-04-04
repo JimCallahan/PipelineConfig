6dec124f6cdaa7696dd36c571b05c26a6ea84aba
Jim Callahan <jim@temerity.us>
2004-04-04 05:35:13 +0000

    new copyright and license agreement

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -rf i686-pc-linux-gnu-dbg
-mkdir  i686-pc-linux-gnu-dbg
+rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
+mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
 
 pushd $HOME/code2/src/plconfig
   sh autogen.sh
@@ -10,6 +10,15 @@ popd
 pushd i686-pc-linux-gnu-dbg
   $HOME/code2/src/plconfig/configure \
     --disable-opt \
+    --with-compiler=GNU \
+    --prefix=/usr \
+    --datadir=/usr/share \
+    --with-plsrc=$HOME/code2/src/pipeline
+popd
+
+pushd i686-pc-linux-gnu-opt
+  $HOME/code2/src/plconfig/configure \
+    --enable-opt \
     --with-compiler=GNU \
     --prefix=/usr \
     --datadir=/usr/share \
