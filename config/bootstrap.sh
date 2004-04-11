1cb46ca35746bb4c3a71e9cbabad2f67090ff391
Jim Callahan <jim@temerity.us>
2004-04-11 22:54:59 +0000

    new license, automatic inclusion of license in installer program

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -7,14 +7,14 @@ pushd $HOME/code2/src/plconfig
   sh autogen.sh
 popd
 
-pushd i686-pc-linux-gnu-dbg
-  $HOME/code2/src/plconfig/configure \
-    --disable-opt \
-    --with-compiler=GNU \
-    --prefix=/usr \
-    --datadir=/usr/share \
-    --with-plsrc=$HOME/code2/src/pipeline
-popd
+# pushd i686-pc-linux-gnu-dbg
+#   $HOME/code2/src/plconfig/configure \
+#     --disable-opt \
+#     --with-compiler=GNU \
+#     --prefix=/usr \
+#     --datadir=/usr/share \
+#     --with-plsrc=$HOME/code2/src/pipeline
+# popd
 
 pushd i686-pc-linux-gnu-opt
   $HOME/code2/src/plconfig/configure \
