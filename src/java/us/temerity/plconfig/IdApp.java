d5314e7e82e05a3a4ec530480e7c7068c1092284
Jim Callahan <jim@temerity.us>
2004-09-01 04:30:34 +0000

    fixed bug where MAC address was not being determined correctly, readded /proc/scsi and /proc/pci checks

--- src/java/us/temerity/plconfig/IdApp.java
+++ src/java/us/temerity/plconfig/IdApp.java
@@ -1,4 +1,4 @@
-// $Id: IdApp.java,v 1.2 2004/08/27 22:18:57 jim Exp $
+// $Id: IdApp.java,v 1.3 2004/09/01 04:30:34 jim Exp $
 
 package us.temerity.plconfig;
 
@@ -101,7 +101,29 @@ class IdApp
       parser.setApp(this);
       parser.CommandLine();
       
-      generateID();
+      TreeMap<String,BigInteger> IDs = generateIDs();
+      {
+	StringBuffer buf = new StringBuffer();
+	for(String host : IDs.keySet()) {
+	  buf.append(host + " ");
+	  
+	  int wk;
+	  for(wk=0; wk<(29 - host.length()); wk++) 
+	    buf.append(" ");
+	  
+	  buf.append(IDs.get(host).toString());
+	  buf.append("\n");    
+	}
+	
+	if(pHostIDs == null) {
+	  System.out.print(buf.toString());
+	}
+	else {
+	  FileWriter writer = new FileWriter(pHostIDs, true); 
+	  writer.write(buf.toString());
+	  writer.close();
+	}
+      }
 
       success = true;
     }
@@ -119,69 +141,141 @@ class IdApp
   /*-- RUN HELPERS -------------------------------------------------------------------------*/
 
   /**
-   * Generate the hardware ID for the local machine.
+   * Generate the hardware IDs for the local machine.
    */
-  private void
-  generateID() 
+  private TreeMap<String,BigInteger>
+  generateIDs() 
     throws IOException 
   {
-    /* determine the hardware ID */ 
-    String cksum = null;
+    /* the names of this host indexed by IP address */ 
+    TreeMap<String,String> hostnames = new TreeMap<String,String>();
+    try {
+      Enumeration nets = NetworkInterface.getNetworkInterfaces();  
+      while(nets.hasMoreElements()) {
+	NetworkInterface net = (NetworkInterface) nets.nextElement();
+	Enumeration addrs = net.getInetAddresses();
+	while(addrs.hasMoreElements()) {
+	  InetAddress addr = (InetAddress) addrs.nextElement();
+	  String ip = addr.getHostAddress();
+	  if(!ip.equals("127.0.0.1")) 
+	    hostnames.put(ip, addr.getCanonicalHostName());
+	}
+      }
+    }
+    catch(Exception ex) {
+      throw new IOException 
+	("Could not determine the name of this machine!");
+    }
+
+    if(hostnames.isEmpty()) 
+      throw new IOException 
+	("Could not determine the name of this machine!");
+
+    /* determine the hardware IDs */ 
+    TreeMap<String,BigInteger> IDs = new TreeMap<String,BigInteger>();
     try {
       MessageDigest md = MessageDigest.getInstance("MD5");
       
       /* get the network card MAC addresses */ 
+      TreeSet<String> found = new TreeSet<String>();
       {
-	FileReader reader = new FileReader("/proc/net/arp");
-	boolean done = false;
-	while(true) {
-	  /* read a line */ 
+	String str = null;
+	try {
+	  String args[] = new String[1];
+	  args[0] = "/sbin/ifconfig";
+	  
+	  String env[] = new String[0];
+	  
+	  Process proc = Runtime.getRuntime().exec(args, env);
+	  
 	  StringBuffer buf = new StringBuffer();
+	  InputStreamReader in = new InputStreamReader(proc.getInputStream());
+	  char cs[] = new char[1024];
 	  while(true) {
-	    int next = reader.read();
-	    if(next == -1) {
-	      done = true;
-	      break;
-	    }
-	    
-	    char c = (char) next;
-	    if(c == '\n') 
+	    int cnt = in.read(cs);
+	    if(cnt == -1) 
 	      break;
 	    
-	    buf.append(c);
+	    buf.append(cs, 0, cnt);
 	  }
 
-	  if(done) 
-	    break;
-	  
-	  String addr = null;
+	  int exitCode = proc.waitFor();
+	  if(exitCode != 0)
+	    throw new IOException();
 
-	  String[] fields = buf.toString().split(" ");
-	  int wk, cnt;
-	  for(wk=0, cnt=0; wk<fields.length; wk++) {
-	    if(fields[wk].length() > 0) {
-	      if((cnt == 3) && (!fields[wk].equals("00:00:00:00:00:00"))) {
-		addr = fields[wk];
-	      }
-	      else if((cnt == 5) && (fields[wk].equals("eth0")) && (addr != null)) {
-		md.update(addr.getBytes());
-		break;
-	      }
+	  str = buf.toString();
+	}
+	catch(Exception ex) {
+	  throw new IOException();
+	}      
+	
+	if(str == null)
+	  throw new IOException();
+	
+	{
+	  boolean hwNext = false;
+	  boolean ipNext = false;
 
-	      cnt++;
+	  String hw = null;
+
+	  String tok[] = str.split(" ");
+	  int wk; 
+	  for(wk=0; wk<tok.length; wk++) {
+	    if(tok[wk].length() > 0) {
+	      if(hwNext) {
+		String[] hex = tok[wk].split(":");
+		if(hex.length != 6) 
+		  throw new IOException();
+
+		int i;
+		for(i=0; i<hex.length; i++) 
+		  if(hex[i].length() != 2)
+		    throw new IOException();
+		
+		hw = tok[wk];
+		hwNext = false;
+	      }
+	      else if(ipNext) {
+		if(tok[wk].startsWith("addr:")) {
+		  String addr = tok[wk].substring(5);
+		  String hname = hostnames.get(addr);
+		  if(hname != null) {
+		    String s = ("IP=" + addr + " HWaddr=" + hw);
+		    md.update(s.getBytes());
+		    found.add(hname);
+		  }
+		}
+		else {
+		  throw new IOException();
+		}
+		
+		hw     = null;
+		ipNext = false;
+	      }
+	      else if(tok[wk].equals("HWaddr")) {
+		hwNext = true;
+	      }
+	      else if(tok[wk].equals("inet")) {
+		ipNext = true;
+	      }
+	      else {
+		hwNext = false;
+		ipNext = false;
+	      }
 	    }
 	  }
 	}
 
-	reader.close();
+	if(found.isEmpty()) 
+	  throw new IOException();
       }
       
       /* get the CPU, PCI amd SCSCI info */ 
       {
 	String[] files = {
-	  "/proc/cpuinfo"
-	  /*	  "/proc/pci", 
-		  "/proc/scsi/scsi" */ 
+	  "/proc/cpuinfo",
+	  "/proc/pci", 
+	  "/proc/scsi/scsi"
 	};
 	  
 	byte[] buf = new byte[4096];
@@ -205,59 +299,15 @@ class IdApp
 	}
       }  
 
-      BigInteger big = new BigInteger(md.digest());
-      cksum = big.toString();
+      BigInteger hardwareID = new BigInteger(md.digest());
+      for(String host : found) 
+	IDs.put(host, hardwareID);
     }
     catch(Exception ex) {
       throw new IOException("Unable to determine local host ID.");
     }      
-    assert(cksum != null);
 
-    /* lookup the names the machine is known by */ 
-    TreeSet<String> hosts = new TreeSet<String>(); 
-    try {
-      Enumeration nets = NetworkInterface.getNetworkInterfaces();  
-      while(nets.hasMoreElements()) {
-	NetworkInterface net = (NetworkInterface) nets.nextElement();
-	Enumeration addrs = net.getInetAddresses();
-	while(addrs.hasMoreElements()) {
-	  InetAddress addr = (InetAddress) addrs.nextElement();
-	  String ip = addr.getHostAddress();
-	  if(!ip.equals("127.0.0.1")) {
-	    hosts.add(addr.getCanonicalHostName());
-	  }	    
-	}
-      }
-    }
-    catch(Exception ex) {
-      throw new IOException 
-	("Could not determine the name of this machine!");
-    }
-
-    if(hosts.isEmpty()) 
-      throw new IOException 
-	("Could not determine the name of this machine!");
-
-    StringBuffer buf = new StringBuffer();
-    for(String host : hosts) {
-      buf.append(host + " ");
-      
-      int wk;
-      for(wk=0; wk<(29 - host.length()); wk++) 
-	buf.append(" ");
-
-      buf.append(cksum);
-      buf.append("\n");    
-    }
-
-    if(pHostIDs == null) {
-      System.out.print(buf.toString());
-    }
-    else {
-      FileWriter writer = new FileWriter(pHostIDs, true); 
-      writer.write(buf.toString());
-      writer.close();
-    }
+    return IDs;
   }
 
 
