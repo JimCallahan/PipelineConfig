b7473c4c1f3ee40fe7011df5e426aa4067b6a601
Jim Callahan <jim@temerity.us>
2008-05-22 09:26:56 +0000

    upgraded for newer automake/autoconf

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+amv=`automake --version | head -1 | awk '{print $4}'`
+echo "Requires: Automake-1.10 (found ${amv})."
+
+acv=`autoconf --version | head -1 | awk '{print $4}'`
+echo "Requires: Autoconf-2.61 (found ${acv})."
+
+
 echo "Building macros."
 aclocal 
 
@@ -7,7 +14,7 @@ echo "Building config header."
 autoheader
 
 echo "Building makefiles."
-automake --add-missing --copy 
+automake --add-missing --copy --warnings=no-portability
 
 echo "Building configure."
 autoconf
